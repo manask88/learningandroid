93d0e9cc93f0a91c95f0c5c4c67d34eacf81a492
Manuel Nakamurakare <manask88@gmail.com>
2011-07-31 23:53:50 -0500

    beginning chapter 10

--- Yamba/src/startup/nsn/UpdaterService.java
+++ Yamba/src/startup/nsn/UpdaterService.java
@@ -4,124 +4,94 @@ import java.util.List;
 
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
-
 import android.app.Service;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.IBinder;
 import android.util.Log;
 
-public class UpdaterService extends Service{
-private static final String TAG = "UpdaterService";
-static final int DELAY = 60000; // minute
-private boolean runFlag = false;
-private Updater updater;
-private YambaApplication yamba;
+public class UpdaterService extends Service {
+  private static final String TAG = "UpdaterService";
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  static final int DELAY = 60000; // wait a minute
+  private boolean runFlag = false;
+  private Updater updater;
+  private YambaApplication yamba;
 
-	@Override
-	public void onCreate() {
-		// TODO Auto-generated method stub
-		super.onCreate();
-		this.yamba = (YambaApplication) getApplication();
-		this.updater = new Updater();
-		Log.d(TAG,"onCreated");
-	}
+  DbHelper dbHelper; // 
+  SQLiteDatabase db;
 
-	@Override
-	public void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-		
-		this.runFlag=false;
-		this.updater.interrupt();
-		this.updater=null;
-		this.yamba.setServiceRunning(false);
-		Log.d(TAG,"onDestroyed");
-	}
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		// TODO Auto-generated method stub
-		super.onStartCommand(intent, flags, startId);
-		this.runFlag = true;
-		this.updater.start();
-		this.yamba.setServiceRunning(true);
-		Log.d(TAG,"onStarted");
-		return START_STICKY;
-	}
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    this.yamba = (YambaApplication) getApplication();
+    this.updater = new Updater();
 
-	
-	/**
-	   * Thread that performs the actual update from the online service
-	   */
-	
-	private class Updater extends Thread {
-		List<Twitter.Status> timeline;
-		public Updater(){
-			super("UpdaterService-Updater");  //to give a name to the thread
-			
-			
-			
-			
-			
-			
-		}
+    dbHelper = new DbHelper(this); // 
 
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			UpdaterService updaterService = UpdaterService.this;
-			while (updaterService.runFlag)
-			{
-				Log.d(TAG,"Updater running");
-				try
-				{
-					//Some work goes here
-					
-					try{
-						timeline = yamba.getTwitter().getFriendsTimeline();
-						
-					}	catch (TwitterException e) {
-						Log.e(TAG,"Failed to connect to twitter service",e);
-						
-						
-					}
-					
-					
-					//Loop over the timeline and print it out
-					for (Twitter.Status status : timeline) {
-						Log.d(TAG,String.format("%s: %s", status.user.name, status.text));
-					}
-					
-					Log.d(TAG,"Updater ran");
-					Thread.sleep(DELAY);
-					
-					
-					
-				} catch (InterruptedException e)
-				{
-					updaterService.runFlag = false;
-					
-					
-				}
-				
-				
-				
-			}
-				
-				
-				
-		}
-		
-		
-		
-		
+    Log.d(TAG, "onCreated");
+  }
+
+  @Override
+  public int onStartCommand(Intent intent, int flag, int startId) {
+    if (!runFlag) {
+      this.runFlag = true;
+      this.updater.start();
+      ((YambaApplication) super.getApplication()).setServiceRunning(true);
+
+      Log.d(TAG, "onStarted");
+    }
+    return Service.START_STICKY;
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+
+    this.runFlag = false;
+    this.updater.interrupt();
+    this.updater = null;
+    this.yamba.setServiceRunning(false);
+
+    Log.d(TAG, "onDestroyed");
+  }
+
+  /**
+   * Thread that performs the actual update from the online service
+   */
+  private class Updater extends Thread {
+
+	  public Updater() {
+	    super("UpdaterService-Updater");
+	  }
+
+	  @Override
+	  public void run() {
+	    UpdaterService updaterService = UpdaterService.this;
+	    while (updaterService.runFlag) {
+	      Log.d(TAG, "Running background thread");
+	      try {
+	        YambaApplication yamba = (YambaApplication) updaterService
+	            .getApplication();  // 
+	        int newUpdates = yamba.fetchStatusUpdates(); // 
+	        if (newUpdates > 0) { // 
+	          Log.d(TAG, "We have a new status");
+	        }
+	        Thread.sleep(DELAY);
+	      } catch (InterruptedException e) {
+	        updaterService.runFlag = false;
+	      }
+	    }
+	  }
 	} // Updater
-	
-	
+  
 }
+
+
--- Yamba/src/startup/nsn/YambaApplication.java
+++ Yamba/src/startup/nsn/YambaApplication.java
@@ -1,7 +1,11 @@
 package startup.nsn;
 
+import java.util.List;
+
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.Twitter.Status;
 import android.app.Application;
+import android.content.ContentValues;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.preference.PreferenceManager;
@@ -12,9 +16,14 @@ public class YambaApplication extends Application implements
 OnSharedPreferenceChangeListener { // 
 	  private static final String TAG = YambaApplication.class.getSimpleName();
 	  public Twitter twitter; // 
+	  private StatusData statusData;
 	  private SharedPreferences prefs;
 	  private boolean serviceRunning;
 
+	  public StatusData getStatusData() {
+		  return statusData;
+	  }
+	  
 	  public boolean isServiceRunning() {
 		return serviceRunning;
 	}
@@ -26,6 +35,7 @@ OnSharedPreferenceChangeListener { //
 	@Override
 	  public void onCreate() { // 
 	    super.onCreate();
+	    statusData = new StatusData(this);
 	    this.prefs = PreferenceManager.getDefaultSharedPreferences(this);
 	    this.prefs.registerOnSharedPreferenceChangeListener(this);
 	    Log.i(TAG, "onCreated");
@@ -41,6 +51,7 @@ OnSharedPreferenceChangeListener { //
 	    if (this.twitter == null) {
 	      String username = this.prefs.getString("username", "");
 	      String password = this.prefs.getString("password", "");
+
 	      String apiRoot = prefs.getString("apiRoot",
 	          "http://yamba.marakana.com/api");
 	      if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)
@@ -56,5 +67,44 @@ OnSharedPreferenceChangeListener { //
 	      SharedPreferences sharedPreferences, String key) { // 
 	    this.twitter = null;
 	  }
+
+	  
+	  
+	// Connects to the online service and puts the latest statuses into DB.
+	// Returns the count of new statuses
+	public synchronized int fetchStatusUpdates() {  // 
+	  Log.d(TAG, "Fetching status updates");
+	  Twitter twitter = this.getTwitter();
+
+	  if (twitter == null) {
+	    Log.d(TAG, "Twitter connection info not initialized");
+	    return 0;
+	  }
+	  try {
+	    List<Status> statusUpdates = twitter.getFriendsTimeline();
+	    long latestStatusCreatedAtTime = this.getStatusData()
+	        .getLatestStatusCreatedAtTime();
+	    int count = 0;
+	    ContentValues values = new ContentValues();
+	    for (Status status : statusUpdates) {
+	      values.put(StatusData.C_ID, status.getId());
+	      long createdAt = status.getCreatedAt().getTime();
+	      values.put(StatusData.C_CREATED_AT, createdAt);
+	      values.put(StatusData.C_TEXT, status.getText());
+	      values.put(StatusData.C_USER, status.getUser().getName());
+	      Log.d(TAG, "Got update with id " + status.getId() + ". Saving");
+	      this.getStatusData().insertOrIgnore(values);
+	      if (latestStatusCreatedAtTime < createdAt) {
+	        count++;
+	      }
+	    }
+	    Log.d(TAG, count > 0 ? "Got " + count + " status updates"
+	        : "No new status updates");
+	    return count;
+	  } catch (RuntimeException e) {
+	    Log.e(TAG, "Failed to fetch status updates", e);
+	    return 0;
+	  }
+	}
 
 	}
